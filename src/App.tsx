8a7db19e72bf06ace7691cb530af5457a4e02643
Lasse Fisker <lasse@cobraid.dk>
2026-04-04 10:14:45 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useRef, type ReactNode } from "react";
 import "./App.css";
 import palae from "./palae.png";
-import church from "./Illustrationer/Tag1.png";
+import church from "./Illustrationer/Tag2.png";
 import garden from "./Illustrationer/Tag3.png";
-import palace from "./Illustrationer/Tag2.png";
+import palace from "./Illustrationer/Tag1.png";
 import sunflower from "./Illustrationer/Solsikke_hel.png";
 import sunfloweProfile from "./Illustrationer/Solsikke_profil.png";
 import leaf from "./Illustrationer/Solsikkeblad.png";
@@ -39,8 +39,8 @@ const Greeting = ({ mobile = false }: { mobile?: boolean }) => {
       <section className="bg-light border-text flex h-fit w-full flex-col gap-8 rounded-2xl border border-solid p-8">
         <h1 className="font-display text-center text-6xl">Anders & Liv</h1>
         <p className="text-center">
-          siger ja til kærligheden og til hinanden. Det vil derfor glæde dem at
-          se dig/jer til deres bryllup
+          Siger ja til kærligheden og til hinanden. Det vil derfor glæde dem at
+          se dig/jer til deres bryllup.
         </p>
       </section>
     </div>
@@ -93,7 +93,7 @@ const locations: Location[] = [
     lng: 10.07041,
     lat: 56.15223,
     img: church,
-    address: "Kirkekirkevej 13, 8220 Branrand",
+    address: "Årslev Kirkevej 4B, 8220 Brabrand",
   },
   {
     name: "Fællesgartneriet i Årslev ",
@@ -174,16 +174,83 @@ function App() {
       <Greeting mobile />
 
       <Content>
+        <Section title="S.U.">
+          <p>
+            Vi håber, at du/I har lyst til at fejre dagen med os, og vil derfor
+            gerne have en tilbagemelding på, om du/I kommer eller ej,{" "}
+            <strong> senest d. 1 juli.</strong>
+            <br />
+            <br />
+            Dette kan gøres på sms til Anders på{" "}
+            <a
+              href="tel:31229499"
+              className="text-accent font-bold underline underline-offset-2"
+            >
+              31229499{" "}
+            </a>{" "}
+            eller Liv på{" "}
+            <a
+              href="tel:29722751"
+              className="text-accent font-bold underline underline-offset-2"
+            >
+              29722751{" "}
+            </a>
+          </p>
+        </Section>
         <Section title="Parkering">
-          <div className="flex gap-4">
-            <Map />
-            <div className="flex flex-col gap-2">
-              <p>Der er mulighed for parkering flere steder:</p>
-              <ul className="ml-4 flex list-disc flex-col gap-2">
-                <li>Kirkens parkering</li>
-                <li>Duelandsvej (parallelt med Silkeborgvej</li>
-                <li>Årslev Markvej (bag palæet)</li>
-              </ul>
+          <div className="flex flex-col gap-2">
+            <p>Der er mulighed for parkering flere steder:</p>
+            <ul className="ml-4 flex list-disc flex-col gap-2">
+              <li>
+                <a
+                  target="_blank"
+                  href="https://l.facebook.com/l.php?u=https%3A%2F%2Fmaps.app.goo.gl%2FoJ7fAUdtVQi5bHpK6%3Fg_st%3Dic%26fbclid%3DIwZXh0bgNhZW0CMTAAYnJpZBEwZWhtdXZxb3ZwSEhOcVVCc3NydGMGYXBwX2lkEDIyMjAzOTE3ODgyMDA4OTIAAR4OvuQPPteRShp3sagiy_ZiScmGLZlcW2E_ZYAkU4wW_HbLrTS2g3PsEXUUpw_aem_4ZyhpBOMDulj1yBWMYbPow&h=AT7uLtknvRRmYa2VDcAdaqaWmgffZyokMV7c82jbEC9y6yO9M4M-0BOQo8TSh8_defrQv4KSYvOEh4v1vWc09X8jw3MfoUh-7JmURe-RJ7hBMNDc0GpwFEka2zEHJMkPxRXW4wR9"
+                  className="text-accent font-bold underline underline-offset-2"
+                >
+                  Kirkens parkering
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://maps.app.goo.gl/cFV8SZVytiGMd2Vs7?g_st=ic"
+                  className="text-accent font-bold underline underline-offset-2"
+                >
+                  Duelandsvej (parallelt med Silkeborgvej
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://l.facebook.com/l.php?u=https%3A%2F%2Fmaps.app.goo.gl%2FMSRJVrVaaTt8wfqv7%3Fg_st%3Dic%26fbclid%3DIwZXh0bgNhZW0CMTAAYnJpZBEwZWhtdXZxb3ZwSEhOcVVCc3NydGMGYXBwX2lkEDIyMjAzOTE3ODgyMDA4OTIAAR4dqKtzGLh_KSAsZAIPg7Esg7MVHF070VViStrvAP6VrAMZRlXFcec_urfguQ_aem_o7Mt9vrafjyYjHD4yUW_hw&h=AT7uLtknvRRmYa2VDcAdaqaWmgffZyokMV7c82jbEC9y6yO9M4M-0BOQo8TSh8_defrQv4KSYvOEh4v1vWc09X8jw3MfoUh-7JmURe-RJ7hBMNDc0GpwFEka2zEHJMkPxRXW4wR9"
+                  className="text-accent font-bold underline underline-offset-2"
+                >
+                  Årslev Markvej (bag palæet)
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Section>
+        <Section title="Praktisk">
+          <div className="flex flex-col gap-4">
+            <div>
+              <h3 className="font-bold">Fodtøj</h3>
+              <p>
+                Egnet til havefest og gang på græs og grussti. Efter vielsen går
+                vi 500 meter fra kirken til receptionen i samlet
+                bryllupsprocession. Efter receptionen går vi 500 meter fra haven
+                til vores hjem i Palæet.
+              </p>
+            </div>
+            <div>
+              <h3 className="font-bold">Påklædning efter vejret</h3>
+              <p>
+                Receptionen holdes under åben himmel, med mulighed for ly i
+                drivhusene og under oversejl. Der vil være mulighed for at gå
+                tur på græs mellem haverne og indenfor i drivhusene. Festen
+                foregår i telt i haven, og det kan blive køligt hen under
+                aftenen.
+              </p>
             </div>
           </div>
         </Section>
@@ -198,7 +265,7 @@ function App() {
         </Section>
         <Section title="Ønsker">
           <p>
-            Skulle du/i have lyst til at give en gave, har Anders og Liv
+            Skulle du/I have lyst til at give en gave, har Anders og Liv
             oprettet en ønskesky til inspiration.
             <br />
             <a
@@ -211,7 +278,7 @@ function App() {
         </Section>
         <Section title="Taler, indslag og spørgsmål">
           <p>
-            Ønsker du/i at holde en tale, eller lave et indslag under
+            Ønsker du/I at holde en tale, eller lave et indslag under
             bryllupsfesten, bedes du række ud til Anders’ bror Lasse, som er
             toastmaster på dagen, på{" "}
             <a
@@ -231,8 +298,8 @@ function App() {
           <div className="flex flex-col gap-2">
             <p>
               I er inviteret til bryllup d. 22 august 2026. Dagen foregår på tre
-              lokationer; Vielse i Årslev kirke, Reception i fællesgartneriet og
-              slutteligt bryllupfest i palæet
+              lokationer; Vielse i Årslev kirke, Reception i Fællesgartneriet og
+              slutteligt bryllupfest i palæet.
             </p>
             <div className="mt-4 flex w-full flex-col justify-between lg:flex-row">
               <Location
